3a953c9052026c6e9a02e217346654fef828501e
seungwooKim99 <sonicdx886@gmail.com>
2020-07-06 09:59:57 +0900

    #isMatched 비동기 처리

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -2,10 +2,12 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
     User: {
-        isMatched: ({ id }) => {
-            let count = prisma.usersConnection({ where: { followers_some: { id } } })
+        isMatched: async (parent, _, { request }) => {
+            const { user } = request;
+            let count = await prisma.usersConnection({ where: { following_some: { id: user.id } } })
                 .aggregate()
                 .count();
+            console.log(count);
             if (count !== 1) {
                 return false;
             }
